c0b08bcbb57fb6e425d51da966ee3c35d3e69e62
Michiaki Ariga <chezou+github@gmail.com>
2014-09-15 14:06:50 +0900

    Add test

--- src/MeCab.jl
+++ src/MeCab.jl
@@ -1,5 +1,82 @@
 module MeCab
 
-# package code goes here
+export Mecab, MecabResult, sparse_tostr, nbest_sparse_tostr, nbest_init, nbest_next_tostr, parse_tonode, parse, parse_surface
 
-end # module
+type Mecab
+  ptr::Ptr{Void}
+
+  function Mecab(option::String = "")
+    argv = split(option)
+    if(length(argv) == 0)
+      argv = [""]
+    end
+
+    ptr = ccall(
+      (:mecab_new, "libmecab"),
+      Ptr{Void},
+      (Cint, Ptr{Ptr{Uint8}}),
+      length(argv), argv
+    )
+
+    if ptr == C_NULL
+      error("failed to create tagger")
+    end
+    smart_p = new(ptr)
+
+    finalizer(smart_p, obj -> ccall((:mecab_destroy, "libmecab"),  Void, (Ptr{Void},), obj.ptr))
+
+    smart_p
+  end
+end
+
+type MecabResult
+  surface::String
+  feature::String
+end
+
+function sparse_tostr(mecab::Mecab, input::String)
+  result = ccall(
+      (:mecab_sparse_tostr, "libmecab"), Ptr{Uint8},
+      (Ptr{Uint8}, Ptr{Uint8},),
+      mecab.ptr, bytestring(input)
+    )
+  bytestring(result)
+end
+
+function nbest_sparse_tostr(mecab::Mecab, n::Int64, input::String)
+  result = ccall(
+      (:mecab_nbest_sparse_tostr, "libmecab"), Ptr{Uint8},
+      (Ptr{Uint8}, Int32, Ptr{Uint8},),
+      mecab.ptr, n, bytestring(input)
+    )
+  bytestring(result)
+end
+
+function nbest_init(mecab::Mecab, input::String)
+  ccall((:mecab_nbest_init, "libmecab"), Void, (Ptr{Void}, Ptr{Uint8}), mecab.ptr, bytestring(input))
+end
+
+function nbest_next_tostr(mecab::Mecab)
+  result = ccall((:mecab_nbest_next_tostr,"libmecab"), Ptr{Uint8}, (Ptr{Void},), mecab.ptr)
+  bytestring(result)
+end
+
+function parse(mecab::Mecab, input::String)
+  results = split(sparse_tostr(mecab, input), "\n")
+  pop!(results)
+  pop!(results)
+  nodes = MecabResult[]
+  for result in results
+    surface, feature = split(result)
+    push!(nodes, MecabResult(surface, feature))
+  end
+
+  nodes
+end
+
+function parse_surface(mecab::Mecab, input::String)
+  results = parse(mecab, input)
+  map((x) -> x.surface, results)
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,28 @@
 using MeCab
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# assume using ipadic
+mecab = Mecab()
+
+results = parse(mecab, "今日の天気は晴れです")
+@test length(results) == 6
+@test isa(results[1], MecabResult)
+@test results[1].surface == "今日"
+@test results[1].feature == "名詞,副詞可能,*,*,*,*,今日,キョウ,キョー"
+
+results = parse_surface(mecab, "今日の天気は晴れです")
+@test length(results) == 6
+@test isa(results[1], String)
+@test results[1] == "今日"
+
+result = sparse_tostr(mecab, "こんにちは")
+@test result == "こんにちは\t感動詞,*,*,*,*,*,こんにちは,コンニチハ,コンニチワ\nEOS\n"
+
+result = nbest_sparse_tostr(mecab, 3, "こんにちは")
+@test result == "こんにちは\t感動詞,*,*,*,*,*,こんにちは,コンニチハ,コンニチワ\nEOS\nこん\t名詞,固有名詞,人名,名,*,*,こん,コン,コン\nに\t助詞,格助詞,一般,*,*,*,に,ニ,ニ\nち\t動詞,自立,*,*,五段・ラ行,体言接続特殊２,ちる,チ,チ\nは\t助詞,係助詞,*,*,*,*,は,ハ,ワ\nEOS\nこん\t動詞,自立,*,*,五段・マ行,連用タ接続,こむ,コン,コン\nに\t助詞,格助詞,一般,*,*,*,に,ニ,ニ\nち\t動詞,自立,*,*,五段・ラ行,体言接続特殊２,ちる,チ,チ\nは\t助詞,係助詞,*,*,*,*,は,ハ,ワ\nEOS\n"
+
+MeCab.nbest_init(mecab, "こんにちは")
+result = MeCab.nbest_next_tostr(mecab)
+@test result == "こんにちは\t感動詞,*,*,*,*,*,こんにちは,コンニチハ,コンニチワ\nEOS\n"
+result = MeCab.nbest_next_tostr(mecab)
+@test result == "こん\t名詞,固有名詞,人名,名,*,*,こん,コン,コン\nに\t助詞,格助詞,一般,*,*,*,に,ニ,ニ\nち\t動詞,自立,*,*,五段・ラ行,体言接続特殊２,ちる,チ,チ\nは\t助詞,係助詞,*,*,*,*,は,ハ,ワ\nEOS\n"
